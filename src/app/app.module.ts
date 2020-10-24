41fe1d5228c2de61768554978b7bec7b5d044dba
nathan denholm <nathandenholm@live.co.uk>
2020-10-24 11:51:15 +0100

    Add compoennts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ShopItemComponent } from './components/shop-item/shop-item.component';
+import { CartComponent } from './components/cart/cart.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    ShopItemComponent,
+    CartComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
